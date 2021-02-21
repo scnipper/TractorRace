f521ba378d911a1bbb60f5f2b4cdbe59d14c3946
Andrew Zhironkin <scnipper@creese.me>
2021-02-21 17:03:21 +0300

    create tractor and control him

--- Assets/Scripts/Common/Units/Tractor.cs
+++ Assets/Scripts/Common/Units/Tractor.cs
@@ -4,6 +4,44 @@ namespace Common.Units
 {
 	public class Tractor : MonoBehaviour
 	{
-		
+		private Rigidbody rb;
+
+		private float addRotation;
+		private void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+		}
+
+		private void FixedUpdate()
+		{
+			var speed = rb.velocity.sqrMagnitude;
+			if(speed < 5000)
+				rb.AddRelativeForce(Force * 200);
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				addRotation = 40;
+			}
+
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				addRotation = -40;
+			}
+			if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+			{
+				addRotation = 0;
+			}
+
+			if (addRotation != 0)
+			{
+				print("add rotation "+addRotation);
+				rb.AddRelativeTorque(new Vector3(0,addRotation,0));
+			}
+		}
+
+		public Vector3 Force { get; set; } = Vector3.forward;
 	}
 }
