6704346c5830f8a6dd9fff93b5fcdba3e4a719fc
Andrew Zhironkin <scnipper@creese.me>
2021-02-21 22:26:31 +0300

    tractor makes cylinder with snow by ladle

--- Assets/Scripts/Common/Units/Tractor.cs
+++ Assets/Scripts/Common/Units/Tractor.cs
@@ -1,25 +1,44 @@
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 namespace Common.Units
 {
 	public class Tractor : MonoBehaviour
 	{
+		public Transform ladle;
+		public Transform cylinderGround;
+		public float maxSizeCylinder = 4;
 		private Rigidbody rb;
 
 		private float addRotation;
 		private Tweener tweenRotate;
+		private TweenerCore<Vector3, Vector3, VectorOptions> ladleMoveTween;
+		private Vector3 ladleLocalPosition;
+		private bool isGroundContact;
+		private Vector3 cylinderPos;
+		private Vector3 saveCylinderScale;
+		private GameObject cylinderGroundGameObject;
 
 		private void Start()
 		{
+			cylinderGroundGameObject = cylinderGround.gameObject;
+			saveCylinderScale = cylinderGround.localScale;
+			cylinderGroundGameObject.SetActive(false);
 			rb = GetComponent<Rigidbody>();
+			ladleLocalPosition = ladle.localPosition;
+			cylinderPos = cylinderGround.localPosition;
+			cylinderGround.DOLocalRotate(new Vector3(40, 0, 90), 0.5f)
+				.SetEase(Ease.Linear)
+				.SetLoops(-1, LoopType.Incremental);
 		}
 
 		private void FixedUpdate()
 		{
 			var speed = rb.velocity.sqrMagnitude;
 			if(speed < 5000)
-				rb.AddRelativeForce(Force * 200);
+				rb.AddRelativeForce(Vector3.forward * 200);
 			
 			if (addRotation != 0)
 			{
@@ -59,9 +78,80 @@ namespace Common.Units
 				StopRotate();
 			}
 
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				ladleMoveTween?.Kill();
+				ladleMoveTween = ladle.DOLocalMove(new Vector3(0,1.46f,ladleLocalPosition.z),0.7f )
+					.SetEase(Ease.Linear)
+					.OnComplete(()=>
+					{
+						cylinderGround.gameObject.SetActive(true);
+						isGroundContact = true;
+					});
+			}
+
+			if (Input.GetKeyUp(KeyCode.Space))
+			{
+				isGroundContact = false;
+
+				ladleMoveTween?.Kill();
+
+				ladleMoveTween = ladle.DOLocalMove(ladleLocalPosition,0.7f ).SetEase(Ease.Linear);
+			}
+
 			
+			UpdateSizeCylinderGround();
+			
+
 		}
 
-		public Vector3 Force { get; set; } = Vector3.forward;
+	
+
+		/// <summary>
+		/// Обновляем размер комка земли перед ковшом
+		/// </summary>
+		private void UpdateSizeCylinderGround()
+		{
+			if (cylinderGroundGameObject.activeSelf)
+			{
+				var scale = cylinderGround.localScale;
+
+				var posCylinder = cylinderGround.localPosition;
+
+
+				float deltaCylinder = Time.deltaTime;
+				if (isGroundContact)
+				{
+					scale.x += deltaCylinder;
+					scale.z += deltaCylinder;
+					if (scale.x >= maxSizeCylinder)
+					{
+						scale.x = maxSizeCylinder;
+						scale.z = maxSizeCylinder;
+					}
+				}
+				else
+				{
+					scale.x -= deltaCylinder;
+					scale.z -= deltaCylinder;
+					if (scale.x <= saveCylinderScale.x)
+					{
+						scale = saveCylinderScale;
+						cylinderGroundGameObject.SetActive(false);
+					}
+				}
+
+
+				float scaleX = scale.x - 1;
+				if (scaleX < 0) scaleX = 0;
+				posCylinder.y = cylinderPos.y + (scaleX / 2);
+				posCylinder.z = cylinderPos.z + (scaleX / 2);
+				cylinderGround.localPosition = posCylinder;
+
+				cylinderGround.localScale = scale;
+			}
+
+		}
+
 	}
 }
