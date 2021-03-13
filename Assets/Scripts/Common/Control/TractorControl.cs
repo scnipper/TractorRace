4961fe2d09664b4bf447490bfbbb8c5853eb73e3
Andrew Zhironkin <scnipper@creese.me>
2021-03-13 22:08:40 +0300

    update normal tractor control

--- Assets/Scripts/Common/Control/TractorControl.cs
+++ Assets/Scripts/Common/Control/TractorControl.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Common.Control.Impl;
 using DG.Tweening;
 using UnityEngine;
@@ -7,18 +8,46 @@ namespace Common.Control
 {
 	public class TractorControl : BaseControl,IPointerDownHandler,IPointerUpHandler
 	{
-		public float timeRotate = 0.2f;
+		public float timeRotate = 0.25f;
 		private Vector2 sizeInput;
 		private float axisHorizontal;
 		private Tweener tween;
+		private Tweener tweenLadle;
+		private float ladleDelta;
+		private bool isGroundContact;
+		private bool isDown;
+		private float delayDown;
 
 		private void Start()
 		{
 			sizeInput = GetComponent<RectTransform>().rect.size;
+			DownLadle();
+			StartCoroutine(DelayDown());
+		}
+
+		private IEnumerator DelayDown()
+		{
+			while (true)
+			{
+				if (delayDown > 0)
+				{
+					delayDown -= Time.deltaTime;
+					if (delayDown <= 0)
+					{
+						if(!isDown) DownLadle();
+					}
+				}
+				
+
+				yield return null;
+			}
 		}
 
 		public void OnPointerDown(PointerEventData eventData)
 		{
+			isDown = true;
+			delayDown = 0;
+			UpLadle();
 			var halfWidth = sizeInput.x / 2;
 			tween?.Kill();
 			if (eventData.position.x > halfWidth)
@@ -31,9 +60,29 @@ namespace Common.Control
 			}
 		}
 		
+		
+		private void UpLadle()
+		{
+			tweenLadle?.Kill();
+			tweenLadle = DOTween.To(value => ladleDelta = value, ladleDelta, 0, 0.15f)
+				.SetEase(Ease.Linear)
+				.OnComplete(() => isGroundContact = false);
+		}
+
+		private void DownLadle()
+		{
+			tweenLadle?.Kill();
+			tweenLadle = DOTween.To(value => ladleDelta = value, ladleDelta, 1, 0.15f)
+				.SetEase(Ease.Linear)
+				.OnComplete(() => isGroundContact = true);
+		}
+		
 
 		public void OnPointerUp(PointerEventData eventData)
 		{
+			isDown = false;
+			delayDown = 0.5f;
+
 			tween?.Kill();
 			tween = DOTween.To(value => axisHorizontal = value, axisHorizontal, 0, timeRotate);
 		}
@@ -45,12 +94,12 @@ namespace Common.Control
 
 		public override float GetVertical()
 		{
-			return 0;
+			return ladleDelta;
 		}
 
 		public override bool IsContactGround()
 		{
-			return false;
+			return isGroundContact;
 		}
 
 		public override void ResetControl()
